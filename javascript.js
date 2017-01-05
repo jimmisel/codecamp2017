5558539ca416b26a9c7a2aaf3f9b9680f6381d3e
Jim Misel <jimmisel@yahoo.com>
2017-01-04 19:55:19 -0500

    Added an external js page

---
+++ javascript.js
@@ -0,0 +1,9 @@
+clicked = function (id) {
+        if (document.getElementById(id).style.textDecoration == "line-through") {
+            document.getElementById(id).style.textDecoration = "";
+        } else {
+            document.getElementById(id).style.textDecoration = "line-through";
+        }
+    };
+    
+
